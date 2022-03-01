fe1cac7f579dcc7b33c33a03e52d308bc69f89a2
John Darwin Tacardon <darwinjohn016@gmail.com>
2022-03-01 13:34:23 +0800

    Tweak Add Operator Function to Function Like Operation Function. Also Add Append Sign, Delete and Clear Function

--- calculator/index.js
+++ calculator/index.js
@@ -1,3 +1,5 @@
+const allBtn = document.querySelectorAll('.calculator-btn button');
+
 const numberBtn = document.querySelectorAll('[data-number]');
 const operatorBtn = document.querySelectorAll('[data-operator]');
 
@@ -8,6 +10,7 @@ const equalsBtn = document.querySelector('[data-equals]');
 const previousInput = document.querySelector('.previous-input');
 const currentInput = document.querySelector('.current-input');
 
+const signBtn = document.querySelector('[data-sign]') 
 
 class Calculator{
     constructor(previousInput,currentInput){
@@ -26,9 +29,18 @@ class Calculator{
         this.operator = undefined;
     }
 
+    delete(){
+        this.currentInput = this.currentInput.slice(0,this.currentInput.length-1);
+    }
+
     appendNumber(e){
+
         this.number = e.target.textContent;
-        this.currentInput += this.number;
+
+        if(this.currentInput.includes('.') && this.number === ".") return;
+
+        else this.currentInput += this.number;
+        
 
     }
 
@@ -36,25 +48,49 @@ class Calculator{
         
         this.operator = e.target.textContent;
         
+        if(this.previousInput === "" && this.currentInput === "") return;
+
         if(this.previousInput === ""){
             this.previousInput = this.currentInput + this.operator;
             this.currentInput = "";
         }
+        
+
+        if((this.previousInput.includes("+") || this.previousInput.includes("-") 
+        || this.previousInput.includes("*") || this.previousInput.includes("/")) 
+        && this.currentInput !== "") {
+
+            let operator = this.previousInput.slice(this.previousInput.length-1);
+
+            if(operator === "+") this.currentInput = parseFloat(this.previousInput) + parseFloat(this.currentInput);
+            else if(operator === "-") this.currentInput = parseFloat(this.previousInput) - parseFloat(this.currentInput);
+            else if(operator === "*") this.currentInput = parseFloat(this.previousInput) * parseFloat(this.currentInput);
+            else if(operator === "/") this.currentInput = parseFloat(this.previousInput) / parseFloat(this.currentInput);
+    
+            this.previousInput = this.currentInput + this.operator;
+            this.currentInput = "";
+        }
+        
+        
     }
 
     operation(){
 
-        this.previousInput = parseFloat(this.previousInput);
-        this.currentInput = parseFloat(this.currentInput);
-
-        if(this.operator === "+") this.currentInput = this.previousInput + this.currentInput;
-        else if(this.operator === "-") this.currentInput = this.previousInput - this.currentInput;
-        else if(this.operator === "*") this.currentInput = this.previousInput * this.currentInput;
-        else if(this.operator === "/") this.currentInput = this.previousInput / this.currentInput;
-
+        if(this.operator === "+") this.currentInput = parseFloat(this.previousInput) + parseFloat(this.currentInput);
+        else if(this.operator === "-") this.currentInput = parseFloat(this.previousInput) - parseFloat(this.currentInput);
+        else if(this.operator === "*") this.currentInput = parseFloat(this.previousInput) * parseFloat(this.currentInput);
+        else if(this.operator === "/") this.currentInput = parseFloat(this.previousInput) / parseFloat(this.currentInput);
+    
         this.previousInput = "";
+        this.currentInput = this.currentInput.toString();
 
     }
+
+    appendSign(){
+        if(this.currentInput.indexOf("-") === 0) this.currentInput = this.currentInput.slice(1,this.currentInput.length);
+        else this.currentInput = "-" + this.currentInput;
+    }
+
 
     display(){
         this.currentInputTxt.value = this.currentInput;
@@ -65,17 +101,31 @@ class Calculator{
 
 const calculator = new Calculator(previousInput,currentInput);
 
+
 numberBtn.forEach(btn => btn.addEventListener('click',e=>{
     calculator.appendNumber(e);
-    calculator.display();
 }))
 
 operatorBtn.forEach(btn => btn.addEventListener('click',e=>{
     calculator.appendOperator(e);
-    calculator.display();
 }))
 
 equalsBtn.addEventListener('click',e=>{
     calculator.operation();
-    calculator.display();
 });
+
+deleteBtn.addEventListener('click',e=>{
+    calculator.delete();
+});
+
+clearBtn.addEventListener('click',e=>{
+    calculator.clear();
+});
+
+signBtn.addEventListener('click',e=>{
+    calculator.appendSign();
+})
+
+allBtn.forEach(btn=>btn.addEventListener('click',e=>{
+    calculator.display();
+}));
